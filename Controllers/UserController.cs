1c37cfaebf28eea2ee414568c2a34a3986ca2567
mohata-pulkit <mohata.pulkit@gmail.com>
2020-08-19 22:33:34 +0530

    crt and flicker

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -52,41 +52,62 @@ namespace GoonGamesOuh.Controllers
 				}
 			}
 			{
-				byte[] salt = new byte[128 / 8];
-				using (var rng = RandomNumberGenerator.Create())
-				{
-					rng.GetBytes(salt);
+				bool validEmail = false;
+				bool validDiscord = false;
+
+				if (user.EmailID.Contains('@'))
+                {
+					validEmail = true;
+                }
+
+                if (user.DiscordUsername.Contains('#'))
+                {
+					validDiscord = true;
+                }
+
+				if(validDiscord && validEmail)
+                {
+					byte[] salt = new byte[128 / 8];
+					using (var rng = RandomNumberGenerator.Create())
+					{
+						rng.GetBytes(salt);
+					}
+					user.PasswordSalt = salt;
+
+					string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+					password: user.Password,
+					salt: salt,
+					prf: KeyDerivationPrf.HMACSHA256,
+					iterationCount: 10000,
+					numBytesRequested: 256 / 8));
+
+					user.PasswordHash = hashed;
+
+					string salty = BitConverter.ToString(salt);
+
+					UserClass writeUser = new UserClass();
+
+					writeUser.FirstName = user.FirstName;
+					writeUser.LastName = user.LastName;
+					writeUser.EmailID = user.EmailID;
+					writeUser.DiscordUsername = user.DiscordUsername;
+					writeUser.PasswordHash = user.PasswordHash;
+					writeUser.PasswordSalt = salty;
+					writeUser.CurrentQuestion = 1;
+					writeUser.CurrentPoints = 0;
+					writeUser.SkipCard = false;
+
+					UserWriter.addUser(writeUser);
+
+					myUser.ConfirmationMessage = "Successfully Registered, Please Log In";
+
+					return RedirectToAction("Index");
 				}
-				user.PasswordSalt = salt;
-
-				string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
-				password: user.Password,
-				salt: salt,
-				prf: KeyDerivationPrf.HMACSHA256,
-				iterationCount: 10000,
-				numBytesRequested: 256 / 8));
-
-				user.PasswordHash = hashed;
-
-				string salty = BitConverter.ToString(salt);
-
-				UserClass writeUser = new UserClass();
-
-				writeUser.FirstName = user.FirstName;
-				writeUser.LastName = user.LastName;
-				writeUser.EmailID = user.EmailID;
-				writeUser.DiscordUsername = user.DiscordUsername;
-				writeUser.PasswordHash = user.PasswordHash;
-				writeUser.PasswordSalt = salty;
-				writeUser.CurrentQuestion = 1;
-				writeUser.CurrentPoints = 0;
-				writeUser.SkipCard = false;
-				
-				UserWriter.addUser(writeUser);
-
-				myUser.ConfirmationMessage = "Successfully Registered, Please Log In";
-
-				return RedirectToAction("Index");
+                else
+                {
+					myUser.ConfirmationMessage = "Please Enter Valid Credentials";
+					return RedirectToAction("Index");
+				}
 			}
 		}
 		[HttpPost]
